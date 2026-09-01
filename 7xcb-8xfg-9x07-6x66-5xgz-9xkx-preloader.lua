7cdc29b51a23483b202bd9cecf05afe598f47887
vFailed <eltoxico2021.1@gmail.com>
2026-09-01 00:04:53 -0400

    Update notification titles and durations in preloader

--- 7xcb-8xfg-9x07-6x66-5xgz-9xkx-preloader.lua
+++ 7xcb-8xfg-9x07-6x66-5xgz-9xkx-preloader.lua
@@ -2,11 +2,18 @@
 -- by 9x19
 
 game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "SnapWare se esta cargando",
+    Title = "PATATA LITE",
+    Text = "CREADO POR 9x19",
+    Icon = "rbxassetid://124144322322655",
+    Duration = 3
+})
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Patata Lite se esta cargando",
     Text = "Espero unos segundos",
-    Icon = "rbxassetid://127953776975745",
+    Icon = "rbxassetid://124144322322655",
     Duration = 5
 })
 
-task.wait(6)
+task.wait(7)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/vFailed/Raw/refs/heads/main/0xbf-7x9z-3x12-6x8%24-loader.lua"))()
