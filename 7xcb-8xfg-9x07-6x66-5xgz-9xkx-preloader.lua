8a88bd75944ec82e04653bc533e938708a918729
vFailed <eltoxico2021.1@gmail.com>
2026-09-01 00:08:18 -0400

    Update preloader notifications and wait times
    Removed icon from notifications and added wait times.

--- 7xcb-8xfg-9x07-6x66-5xgz-9xkx-preloader.lua
+++ 7xcb-8xfg-9x07-6x66-5xgz-9xkx-preloader.lua
@@ -4,16 +4,17 @@
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "PATATA LITE",
     Text = "CREADO POR 9x19",
-    Icon = "rbxassetid://124144322322655",
     Duration = 3
 })
+
+task.wait(2)
 
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "Patata Lite se esta cargando",
     Text = "Espero unos segundos",
-    Icon = "rbxassetid://124144322322655",
     Duration = 5
 })
 
 task.wait(7)
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/vFailed/Raw/refs/heads/main/0xbf-7x9z-3x12-6x8%24-loader.lua"))()
